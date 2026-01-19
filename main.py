06c083277c05c7c104c21291cfac7ee2bf33fb2b
Julija Filipovic <filipovicjulija98@gmail.com>
2026-01-19 22:28:49 +0100

    create an agents and add searh possibility!

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 import os
 from langchain_core.prompts import PromptTemplate
-from langchain_groq import ChatGroq
 from langchain_ollama import ChatOllama
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
+from tavily import TavilyClient
+
+
 from dotenv import load_dotenv
 load_dotenv()
 
+tavily = TavilyClient()
+# Define a simple search tool
+# always add the description for the tool as it helps the agent to decide when to use it
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over the internet.
+    Args:
+        query: The query to search for.
+    Returns:
+        The search result
+    """
+    print(f"Search results for '{query}'")
+    return tavily.search(query)
+
+llm = ChatOllama(
+        model="qwen3:0.6b",  
+        max_retries=0
+    )
+tools = [search]
+agent = create_agent(model = llm, tools = tools)
 
 def main():
-    print("Hello from langchain-course!")
-    information = """
-    Elon Musk is a businessman who leads Tesla, SpaceX, and xAI.
-    Born in South Africa in 1971, he's now the world's wealthiest person.
-    """
-    summary_template = """Given the information {information} about a person, I want you to create:
-1. A short summary
-2. 2 interesting facts about them"""
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    ) # it is better then using fstring as it will be easier to debug if prompt is broken
-    
-    # llm = ChatGroq(model="llama-3.1-8b-instant", temperature=0, max_retries=0)
-    llm = ChatOllama(model="gemma3:270m", temperature=0, max_retries=0)
-    # runnable chain
-    # summary_prompt_template -> llm
-    chain = summary_prompt_template | llm # langchain expression language
-    response = chain.invoke({"information": information})
-    print("Response from LLM:", response.content)
-
+    print('Hello!')
+    result = agent.invoke(
+        {
+            "messages": [HumanMessage(content="What's the weather like in Tokyo?")]
+        }
+    )
+    print(f"Agent result: {result}")
 if __name__ == "__main__":
     main()
