fc04ad20baa94c1dcdae9b90eb681d028a0edcd0
Julija Filipovic <filipovicjulija98@gmail.com>
2026-02-02 15:59:43 +0100

    first react agent

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model='gpt-4')
+react_prompt = hub.pull('hwchase17/react')
+agent = create_react_agent(
+    llm = llm,
+    tools = tools,
+    prompt = react_prompt
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
 
 def main():
-    print("Hello from langchain-course!")
+    result = chain.invoke(
+        input ={
+            "input": "search for 2 job postings for python developer roles in Belgrade using langchain on linkedin and list their details.",
+        }
+    )
+    print(result)
 
-
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
+ 
